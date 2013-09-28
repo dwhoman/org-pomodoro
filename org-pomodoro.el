1c64976cb325a0402c7375a8a97942830da2a983
dwhoman <devinwh7@gmail.com>
2013-09-28 14:39:35 -0800

    Initial commit.

--- org-pomodoro.el
+++ org-pomodoro.el
@@ -1,17 +1,84 @@
+;;; org-pomodoro.el --- pomodoro timing in org mode
+
+;; Copyright (C) 2013 Devin Homan.
+
+;; Authors: Devin Homan
+
+;; This file is not currently part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; For commentary, see README.org.
+
+
 (require 'org-timer)
 (require 'org-clock)
 
-(defvar org-pomodoro-work-time 25 "Pomodoro time in minutes.")
-(defvar org-pomodoro-break-time 5 "Pomodoro break time in minutes.")
-(defvar org-pomodoro-long-break-time 30 "Pomodoro break time between sets, in minutes.")
-(defvar org-pomodoro-clock-out-sound nil
-  "Audio to play when pomodoro work-time is finished.")
-(defvar org-pomodoro-clock-in-sound nil
-  "File path to audio to play when pomodoro break is finished.")
-(defvar org-pomodoro-break-file nil
-  "File to record pomodoro break times.")
-(defvar org-pomodoro-break-target "pomodoro-break"
-  "The Org target in the `org-pomodoro-break-file' to record break times.")
+(defgroup org-pomodoro nil
+  "Options concerning clocking working time in Org-mode using Pomodoros."
+  :tag "Org Pomodoro"
+  :group 'org-pomodoro)
+
+(defcustom org-pomodoro-work-time 25
+  "Pomodoro time in minutes."
+  :group 'org-pomodoro
+  :type '(integer))
+
+(defcustom org-pomodoro-break-time 5
+  "Pomodoro break time in minutes."
+  :group 'org-pomodoro
+  :type '(integer))
+
+(defcustom org-pomodoro-long-break-time 30
+  "Pomodoro break time between sets, in minutes."
+  :group 'org-pomodoro
+  :type '(integer))
+
+(defcustom org-pomodoro-clock-out-sound nil
+  "Audio to play when pomodoro work-time is finished."
+  :group 'org-pomodoro
+  :type '(choice
+	  (const :tag "No sound" nil)
+	  (const :tag "Standard beep" t)
+	  (file  :tag "Play sound file"))
+  :link '(variable-link org-clock-sound))
+(defcustom org-pomodoro-clock-out-sound nil
+  "Audio to play when pomodoro work-time is finished."
+  :group 'org-pomodoro
+  :type '(choice
+	  (const :tag "No sound" nil)
+	  (const :tag "Standard beep" t)
+	  (file  :tag "Play sound file"))
+  :link '(variable org-clock-sound))
+(defcustom org-pomodoro-clock-in-sound nil
+  "File path to audio to play when pomodoro break is finished."
+  :group 'org-pomodoro
+  :type '(choice
+	  (const :tag "No sound" nil)
+	  (const :tag "Standard beep" t)
+	  (file  :tag "Play sound file"))
+  :link '(variable-link org-clock-sound))
+(defcustom org-pomodoro-break-file nil
+  "File to record pomodoro break times."
+  :group 'org-pomodoro
+  :type '(choice
+	  (const :tag "Temporary file" nil)
+	  (file :tag "Store break times in a file.")))
+(defcustom org-pomodoro-break-target "pomodoro-break"
+  "The Org target in the `org-pomodoro-break-file' to record break times."
+  :group 'org-pomodoro
+  :type '(string))
 
 (defun org-pomodoro-start ()
   "When the current item has the :POMODORO: property set, start a new pomodoro timer."
@@ -40,8 +107,8 @@
         (when (> org-pomodoro-work-time 
                  (+
                   (* 60 
-                     (string-to-int (match-string 1))) ;hours
-                  (string-to-int (match-string 2)))) ;minutes
+                     (string-to-number (match-string 1))) ;hours
+                  (string-to-number (match-string 2)))) ;minutes
           (end-of-line)
           (open-line 1)
           (forward-line)
@@ -53,7 +120,8 @@
           ;;If the `org-pomodoro-break-file' does not exist,
           ;;generate a file for it and save the path to
           ;;`org-pomodoro-break-file'.
-          (unless (and (stringp org-pomodoro-break-file) 
+          (unless (and (stringp org-pomodoro-break-file)
+		       (< 0 (length org-pomodoro-break-file))
                        (file-readable-p org-pomodoro-break-file)
                        (file-writable-p org-pomodoro-break-file))
             (let ((break-file 
@@ -64,7 +132,10 @@
                                           temporary-file-directory))
                     nil ".org")))
               (with-temp-buffer
-                (insert "* Pomodoro break\n#<<pomodoro-break>>")
+		(unless (< 0 (length org-pomodoro-break-target))
+		  (setq org-pomodoro-break-target (car (get 'org-pomodoro-break-target 'standard-value))))
+		(insert (format "* Pomodoro break\n#<<%s>>" org-pomodoro-break-target))
+
                 (write-file break-file))
               (setq org-pomodoro-break-file break-file)))
           (find-file org-pomodoro-break-file)
@@ -84,3 +155,5 @@
 (add-hook 'org-clock-in-hook 'org-pomodoro-start)
 (add-hook 'org-clock-out-hook 'org-pomodoro-break)
 (add-hook 'org-clock-cancel-hook 'org-pomodoro-cancel)
+
+(provide 'org-pomodoro)
